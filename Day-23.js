1b594b7e534c01ec158746250a35ea1101657b70
amoltere2121 <amoltere2121@gmail.com>
2023-06-04 15:42:20 +0530

    html

---
+++ Day-23.js
@@ -0,0 +1,11 @@
+Q . Print number from given range ,if target is in that limit .. 
+
+var start=3456;
+var end=876
+target=499;
+
+function printNumber(start,end,target) {
+      consol
+}
+printNumber(start,end,target);
+
